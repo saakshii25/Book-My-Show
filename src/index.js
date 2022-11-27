990d6b8e937bc5fb025e6e193a7666fc0ca16cce
saakshii25 <sakshijagdale2003@gmail.com>
2022-11-27 22:02:51 +0530

    added the modules

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
+
+import MovieProvider from "./context/Movie.context";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <MovieProvider>
+        <App />
+      </MovieProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
